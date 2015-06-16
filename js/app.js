9cd1f6de357f904c6173291ae09701c42eddf9ed
lazyexpert <lazyexpert@i.ua>
2015-06-16 21:08:23 +0300

    Started refactoring js..

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ app.controller('multiplyAppController', function ($scope) {
     $scope.vocabulary.current = lang;    
   };
 
-  //Navigation control
+  // Navigation control
   $scope.tab = 1;
   
   $scope.selectTab = function (setTab) {
@@ -21,7 +21,7 @@ app.controller('multiplyAppController', function ($scope) {
     return $scope.tab === checkTab;
   };
 
-  //Help page implementation
+  // Help page implementation
   $scope.activeHelp = 1;
 
   $scope.showHelp = function (num) {
@@ -32,22 +32,83 @@ app.controller('multiplyAppController', function ($scope) {
     return $scope.activeHelp === num;
   }
 
-  //Game Options setting to default
+  // Game Options setting to default
   $scope.options = {
-    "difficulty" : "easy",
-    "operations" : {
-      "Add" : true,
+    "difficulty"        : "easy",
+    "operations"        : {
+      "Add"       : true,
       "Substract" : true,
-      "Multiply" : true,
-      "Divide" : true
+      "Multiply"  : true,
+      "Divide"    : true
     },
     "onlyMultiplyTable" : true,
-    "maxValue" : 100
+    "maxValue"          : 100
   }; 
+
+  // Game status
+  $scope.game = {
+    "started"    : false,
+    "timerValue" : 60,
+    "score"      : 0,
+    "question"   : "? ? ?",
+    "message"    : "If all options are set up, then you may start!",
+    "wrong"      : ""
+  };
+
+  // Handle Start Button click
+  $scope.startGame = function () {    
+    
+    if($scope.game.timer) clearTimeout($scope.game.timer);
+    
+    $scope.game.score = 0;
+    $scope.game.wrong = "";    
+    $scope.game.message = "The game started!";
+    
+    $scope.game.timer = setInterval(function() {      
+      $scope.game.timerValue -= 1;
+      if( $scope.game.timerValue <= 0)
+      {
+        $scope.game.message = "Defeat! Time is out! Your score is " + $scope.game.score;
+        clearTimeout($scope.game.timer);        
+      }
+      $scope.$apply();
+    },1000);
+  };
+
+  // Hanle Go button click, check user answer modify score, generate new question
+  $scope.nextQuestion = function() {
+    var answer = parseInt(document.getElementById('user_answer').value);
+    //TODO ....
+    /*if(correctAnswer == parseInt(answerLabel.value))
+    {
+      userMessage.style.color = "green";
+      userMessage.innerHTML = "Correct!";
+      score ++;
+      timerValue += difficulty;
+    } else {
+      userMessage.style.color = "red";
+      userMessage.innerHTML = "Wrong!";
+      alert(questionLabel.innerHTML + " = " + correctAnswer.toString());
+      questions.push(questions[random]);
+      addWrong(questionLabel.innerHTML, correctAnswer.toString());
+    }
+    scoreLabel.innerHTML = score.toString();
+    correctAnswer = randomQuestion();
+    answerLabel.value = "";
+    answerLabel.focus();*/
+  };
+
 });
 
+// Adding an element to the "wrong-list"
+var addWrong = function(question, answer) {
+  var node = document.createElement('li');
+  var textnode = document.createTextNode(question + " = " + answer);
+  node.appendChild(textnode);
+  wrongList.appendChild(node);
+}
+/*
 // Cache some DOM-elements
-var startButton;
 var goButton;
 var scoreLabel;
 var timerLabel;
@@ -58,23 +119,9 @@ var answerLabel;
 var userMessage;
 var wrongList;
 
-// Some globals (upper scope)
-var correctAnswer;
-var timerValue;
-var timer;
-var score = 0;
-var questions = [];
-var random;
-
-// Options
-var difficulty = 0; // seconds to add on correct answer ( easy || middle || hard )
-var mode = 0; //Game Mode: 1 - multiply only, 2 - multiply + divide ( 1 || 2 )
-
-
-
 window.onload = function()
 {
-  //Init DOM-elements
+  // Init DOM-elements
   startButton = document.getElementById('start');
   goButton = document.getElementById('go');
   scoreLabel = document.getElementById('score');
@@ -86,38 +133,7 @@ window.onload = function()
   modeRadio = document.getElementsByName('mode');
   wrongList = document.getElementById('wrongList');
   
-  GenerateQuestions();
-  userMessage.innerHTML = "If all options are set up, then you may start!";
-
-  startButton.onclick = function()
-  { 
-    if(timer) clearTimeout(timer);
-
-    timerValue = 60;
-    score = 0;
-    wrongList.innerHTML = "";
-    scoreLabel.innerHTML = "";
-
-    if(!Init()) return;
-    userMessage.style.color = "black";
-    userMessage.innerHTML = "The game started!";
-    timer = setInterval(function() {
-      timerValue--;
-      timerLabel.innerHTML = timerValue.toString();
-      if(timerValue == 0)
-      {
-        userMessage.innerHTML = "Defeat! Time is out! Your score is " + score.toString();
-        clearTimeout(timer);
-      }
-    },1000);
-    answerLabel.value = "";
-    answerLabel.focus();
-  };  
-
-  goButton.onclick = function()
-  {
-    if(timer) userPressedGo();        
-  };
+  GenerateQuestions();  
 };
 
 function GenerateQuestions() 
@@ -142,36 +158,6 @@ window.onkeyup = function(e) {
   if(e.keyCode == 13 && timerValue) userPressedGo(); 
 };
 
-//Initialize picked options
-var Init = function() {
- 
-  for(var t = 0; t < diffRadio.length; t++)
-    if(diffRadio[t].checked) {
-      if(diffRadio[t].value == "easy") difficulty = 15;
-      else if(diffRadio[t].value == "middle") difficulty = 10;
-      else if(diffRadio[t].value == "hard") difficulty = 5;
-      break;
-    }
-  if(!difficulty) {
-    alert("Pick difficulty, please");
-    return false;
-  }
-
-  for(var t = 0; t < modeRadio.length; t++)
-    if(modeRadio[t].checked) {
-      mode = parseInt(modeRadio[t].value);
-    }
-  if(!mode) {
-    alert("Pick mode, please");
-    return false;
-  }
-  
-  if(difficulty && mode) {
-    correctAnswer = randomQuestion();
-  }   
-  return true;
-};
-
 // Generate random question
 var randomQuestion = function() {
   var max = questions.length;
@@ -191,31 +177,4 @@ var randomQuestion = function() {
   }  
 };
 
-//User pressed go handler
-var userPressedGo = function() {
-  if(correctAnswer == parseInt(answerLabel.value))
-  {
-    userMessage.style.color = "green";
-    userMessage.innerHTML = "Correct!";
-    score ++;
-    timerValue += difficulty;
-  } else {
-    userMessage.style.color = "red";
-    userMessage.innerHTML = "Wrong!";
-    alert(questionLabel.innerHTML + " = " + correctAnswer.toString());
-    questions.push(questions[random]);
-    addWrong(questionLabel.innerHTML, correctAnswer.toString());
-  }
-  scoreLabel.innerHTML = score.toString();
-  correctAnswer = randomQuestion();
-  answerLabel.value = "";
-  answerLabel.focus();
-};
-
-//Adding an element to the "wrong-list"
-var addWrong = function(question, answer) {
-  var node = document.createElement('li');
-  var textnode = document.createTextNode(question + " = " + answer);
-  node.appendChild(textnode);
-  wrongList.appendChild(node);
-}
+*/
